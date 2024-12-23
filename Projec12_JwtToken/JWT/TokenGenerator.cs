55752763a2a50a79a3caa67d518887af2a04fb7d
Emir <87826166+thisWhe@users.noreply.github.com>
2024-12-23 16:16:25 +0300

    TokenGenerator.cs

--- Projec12_JwtToken/JWT/TokenGenerator.cs
+++ Projec12_JwtToken/JWT/TokenGenerator.cs
@@ -12,52 +12,52 @@ namespace Projec12_JwtToken.JWT
     public class TokenGenerator
     {
         public string GenereateJwtToken(string username, string email, string name, string surname)
-        { // securityKey Token için oluşturdugumuz imza gibi key olusturmak gibi
+        { 
             var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes
                 ("20Derste20ProjeToken+-*/1234tokenJWT"));
 
-            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256); // securityKeyi ve token olustururken sifreleme algoritmasını tutuyo 
-            var claimExample = new[] // bu dizi ise tokenın temel parametlerimizi tutuyo
-            {                        // sub id icin kullanılan parametre
-                new Claim(JwtRegisteredClaimNames.Sub,username), // id
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);  
+            var claimExample = new[] 
+            {                        
+                new Claim(JwtRegisteredClaimNames.Sub,username), 
                 new Claim(JwtRegisteredClaimNames.Email,email),
-                new Claim(JwtRegisteredClaimNames.GivenName,name),  // given name isim 
-                new Claim(JwtRegisteredClaimNames.FamilyName,surname), // familyname soyad
+                new Claim(JwtRegisteredClaimNames.GivenName,name),  
+                new Claim(JwtRegisteredClaimNames.FamilyName,surname), 
                 new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
 
             };
 
-            var token = new JwtSecurityToken( // bu token değişkeni ise tokenımızın yayıncısını ve dinleyicisini
-                issuer: "localhost", // token yayıncısı yani kim tarafından olusturuldu
-                audience: "localhost", // token dinleyicisi kim dinliyo
-                claims: claimExample, // bu tokenın parametreleri nereden geliyo claimExampledan geliyor
-                expires: DateTime.Now.AddMinutes(5), // token ne zamana kadar gecerli
-                signingCredentials: credentials); // tokenın sifreleme algoritmasının i neler
+            var token = new JwtSecurityToken( 
+                issuer: "localhost", 
+                audience: "localhost", 
+                claims: claimExample, 
+                expires: DateTime.Now.AddMinutes(5),
+                signingCredentials: credentials); 
             
-            return new JwtSecurityTokenHandler().WriteToken(token); // tokenımız olustu
+            return new JwtSecurityTokenHandler().WriteToken(token); 
         }
 
         public string GenereateJwtToken2(string username)
-        { // securityKey Token için oluşturdugumuz imza gibi key olusturmak gibi
+        { 
             var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes
                 ("20Derste20ProjeToken+-*/1234tokenJWT"));
 
-            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256); // securityKeyi ve token olustururken sifreleme algoritmasını tutuyo 
-            var claimExample = new[] // bu dizi ise tokenın temel parametlerimizi tutuyo
-            {                        // sub id icin kullanılan parametre
-                new Claim(JwtRegisteredClaimNames.Sub,username), // id
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256); 
+            var claimExample = new[] 
+            {                        
+                new Claim(JwtRegisteredClaimNames.Sub,username), 
                 new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
 
             };
 
-            var token = new JwtSecurityToken( // bu token değişkeni ise tokenımızın yayıncısını ve dinleyicisini
-                issuer: "localhost", // token yayıncısı yani kim tarafından olusturuldu
-                audience: "localhost", // token dinleyicisi kim dinliyo
-                claims: claimExample, // bu tokenın parametreleri nereden geliyo claimExampledan geliyor
-                expires: DateTime.Now.AddMinutes(5), // token ne zamana kadar gecerli
-                signingCredentials: credentials); // tokenın sifreleme algoritmasının i neler
+            var token = new JwtSecurityToken( 
+                issuer: "localhost", 
+                audience: "localhost", 
+                claims: claimExample,
+                expires: DateTime.Now.AddMinutes(5), 
+                signingCredentials: credentials); 
 
-            return new JwtSecurityTokenHandler().WriteToken(token); // tokenımız olustu
+            return new JwtSecurityTokenHandler().WriteToken(token); 
         }
 
 
